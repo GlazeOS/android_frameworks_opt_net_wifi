de331e20aa50a63ec6afbdcdd1a34ed6ba8a25ef
Paul Zhang <paulz@codeaurora.org>
2014-07-18 12:14:11 -0700

    Wifi: Fix ap list issue while wifi turns on
    Decrease the sleep time to resolve the synchronization issue
    which happens between wpa_supplicant and Wifimonitor thread
    when wifi turns on.
    
    Change-Id: I4e2414301ace26a26351b6c1345e1ff6e7ab49a9
    CRs-Fixed: 673012

--- service/java/com/android/server/wifi/WifiMonitor.java
+++ service/java/com/android/server/wifi/WifiMonitor.java
@@ -597,9 +597,9 @@ public class WifiMonitor {
                 new MonitorThread(mWifiNative.getLocalLog()).start();
                 return true;
             }
-            if (connectTries++ < 5) {
+            if (connectTries++ < 50) {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(100);
                 } catch (InterruptedException ignore) {
                 }
             } else {
